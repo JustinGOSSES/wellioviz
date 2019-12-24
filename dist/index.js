c9f80b9ce10c559fe97d33da2a4a32b9c6d01746
JustinGOSSES <jgosses82@gmail.com>
2019-12-24 16:09:45 -0600

    documentation in progress, work from plane

--- dist/index.js
+++ dist/index.js
@@ -52,6 +52,21 @@ module.exports = {
   /** 
    * getExampleTemplate is a function that provides an example template for what to supply to the curveBox function further down.
    * @returns {array} returns an array that contains a single object that is the template for what will be given to the curveBox function for creating the SVG.
+   * EXAMPLE: = [
+      {"multipleLines":"yes",
+      "curveNames":["GR"],
+      "curveColors":["pink"],
+      "fill":[
+        {"curveName":"GR","fill":"yes","fillDirection":"left","cutoffs":[0,ShaleSiltCutOff,SiltSandCutOff],"fillColors":["gray","orange","yellow"],"curve2":""},
+        {"curveName":"RESD","fill":"no","fillDirection":"left","cutoffs":[],"fillColors":[],"curve2":""}
+      ],
+      "curveUnits":["units2","other units"],
+      "data":well_log_curves_reformatted_for_d3_2,
+      "width":200,
+      "height":400,
+      "margin":({top: 20, right: 3, bottom: 30, left: 30}),
+      "depth_curve_name":"DEPTH"}
+      ]
   */
   getExampleTemplate: function (){
     return [
@@ -70,6 +85,30 @@ module.exports = {
       "depth_curve_name":"DEPTH"}
       ]
   },
+
+  /** 
+   * getFakeIncomingSparseDataExample is a function that takes nothing and returns a JSON of fake sparse incoming data. This is much less than the JSON wellio gives when it converts a LAS file into a JSON. This is one of the type examples of data input. 
+   * It is used next by funtion ____ and ___. 
+   * @returns {array} returns an array that contains an object. probably just a single object? Many of the things like max and min depth that are auto-calculated when the input is a wellio JSON and instead explicitly defined here. This saves data transmission from a backend as well as front-end calculation time. 
+  */
+ getFakeIncomingSparseDataExample: function () {
+  data2 = [{
+    "api_no_14": "7376",
+    "curve_type": "RHOB",
+    "curve_values": [2.2322, 2.2513, 2.2548, 2.2445, 2.2223, 2.2047, 2.198, 2.2088, 2.2248, 2.2399, 2.251, 2.255, 2.2526,2.2322, 2.2513, 2.2548, 2.2445, 2.2223, 2.2047, 2.198, 2.2088, 2.2248, 2.2399, 2.251, 2.255, 2.2526,2.2322, 2.2513, 2.2548, 2.2445, 2.2223, 2.2047, 2.198, 2.2088, 2.2248, 2.2399, 2.251, 2.255, 2.2526,2.2322, 2.2513, 2.2548, 2.2445, 2.2223, 2.2047, 2.198, 2.2088, 2.2248, 2.2399, 2.251, 2.255, 2.2526,2.2322, 2.2513, 2.2548, 2.2445, 2.2223, 2.2047, 2.198, 2.2088, 2.2248, 2.2399, 2.251, 2.255, 2.2526,2.2322, 2.2513, 2.2548, 2.2445, 2.2223, 2.2047, 2.198, 2.2088, 2.2248, 2.2399, 2.251, 2.255, 2.2526,2.2322, 2.2513, 2.2548, 2.2445, 2.2223, 2.2047, 2.198, 2.2088, 2.2248, 2.2399, 2.251, 2.255, 2.2526],
+    "linecolor": "rgb(205,0,0,1)",
+    "logscale": false,
+    "max_depth": "1607.3",
+    "min_depth": "1598.3",
+    "null_value": -999.25,
+    "step": 0.1,
+    "units": "g/cc",
+    "x_max": "3",
+    "x_min": "2",
+    }]
+  return data2
+},
+
   ///////////////////////////////
   //// Functions for getting data from LAS files and reformatting to a wellio.js style JSON. 
   ///////////////////////////////
@@ -112,6 +151,21 @@ module.exports = {
   //// Functions for getting basic information out of wellio.js style JSON for plotting
   ///////////////////////////////
 
+  /////// this require wellio!
+  fileToJSON:function (afile){
+    return wellio.las2json(afile)
+  },
+  /////////
+  turnFilesIntoTextIntoWellioJSON:function (files_array){
+    //// For each well log file, turn into text, then convert text into wellio style JSON using wellio.js
+    let logs_in_json = []
+    for (let i = 0; i < files_array.length; i++) {
+      logs_in_json.push(fileToJSON(files_array[i]))
+    }
+    return logs_in_json
+  },
+
+
   /** 
    * fromJSONofWEllGetThingsForPlotting is a function that takes in wellio style JSON of all LAS file well log information, 
    * and returns an object that contains 3 things in an object format that are used in function ___ for plotting.
@@ -134,22 +188,16 @@ module.exports = {
   //// Functions for reformatting data other than wellio.js style JSON
   ///////////////////////////////
 
-  ////// fake non wellio.js incoming data example
-  // data2 = [{
-  //   "api_no_14": "7376",
-  //   "curve_type": "RHOB",
-  //   "curve_values": [2.2322, 2.2513, 2.2548, 2.2445, 2.2223, 2.2047, 2.198, 2.2088, 2.2248, 2.2399, 2.251, 2.255, 2.2526,2.2322, 2.2513, 2.2548, 2.2445, 2.2223, 2.2047, 2.198, 2.2088, 2.2248, 2.2399, 2.251, 2.255, 2.2526,2.2322, 2.2513, 2.2548, 2.2445, 2.2223, 2.2047, 2.198, 2.2088, 2.2248, 2.2399, 2.251, 2.255, 2.2526,2.2322, 2.2513, 2.2548, 2.2445, 2.2223, 2.2047, 2.198, 2.2088, 2.2248, 2.2399, 2.251, 2.255, 2.2526,2.2322, 2.2513, 2.2548, 2.2445, 2.2223, 2.2047, 2.198, 2.2088, 2.2248, 2.2399, 2.251, 2.255, 2.2526,2.2322, 2.2513, 2.2548, 2.2445, 2.2223, 2.2047, 2.198, 2.2088, 2.2248, 2.2399, 2.251, 2.255, 2.2526,2.2322, 2.2513, 2.2548, 2.2445, 2.2223, 2.2047, 2.198, 2.2088, 2.2248, 2.2399, 2.251, 2.255, 2.2526],
-  //   "linecolor": "rgb(205,0,0,1)",
-  //   "logscale": false,
-  //   "max_depth": "1607.3",
-  //   "min_depth": "1598.3",
-  //   "null_value": -999.25,
-  //   "step": 0.1,
-  //   "units": "g/cc",
-  //   "x_max": "3",
-  //   "x_min": "2",
-  //   }]
 
+ /** 
+   * createDepthArray is a function that takes in a min float, max float, and step float value.  
+   * and returns an array or depth values from the min to the max value going by the step value each step. 
+   * This function is used to create a depth array for plotting when only the max, min, and depth is given explicitly in the input data. This might be done to avoid sending the depth curve from the backend to the front-end. 
+   * @param {float} min a float or integer that represents the top depth of an eventual array of depth values that this function creates.
+   * @param {float} max a float or integer that represents the bottom depth of an array of depth values this function creates.
+   * @param {float} step a float or integer that represents the interval the depth curve increases as you go from the top to the bottom of the depth curve this function creates. 
+   * @returns {array} returns an array of depth values from the min to the max by the step. AN EXAMPLE = [10,10.5,11,11.5,12]
+  */
  createDepthArray: function (min,max,step){
   //// Returns an array of depth values from min to max, including both, with each being different by step value
   //// ran like: depthArray = createDepthArray(data2[0]["min_depth"],data2[0]["max_depth"],data2[0]["step"])
@@ -166,7 +214,10 @@ module.exports = {
   return depth
 },
 
-
+/** 
+ * takeInArraysAndGetObjectOfCurveDataForPlotting is a function used to reformt arrays of curve values into a form that d3.js likes better, an array of objects.
+* THIS FUNCTION NEEDS CHANGED IT IS TOO EXPLICIT !!!!!!!
+*/
 takeInArraysAndGetObjectOfCurveDataForPlotting: function (arraysOfCurvesAndNames,CurveName){
   //// would be run like: reformattedForWelliovizCurveData = takeInArraysAndGetObjectOfCurveDataForPlotting([{"depth":depthArray,"RHOB":data2[0].curve_values}],"RHOB")
   // [{"depth":[],"curveData":[]}]
@@ -190,20 +241,30 @@ takeInArraysAndGetObjectOfCurveDataForPlotting: function (arraysOfCurvesAndNames
   //// Actual plotting functions after all the necessary information is compiled in necessary format
   ///////////////////////////////
 
+  /**
+   * CurveBox2 is the central function to wellioviz in a lot of ways, not least as it holds the d3.js code. It takes a JSOn template, appends the resulting SVG to a defined DIV.
+   * @param {object} well_curve_config_template 
+   * @returns {*} SVG.node() But its main function is to append this SVG to a DIV given in the template that is the single parameter.
+   */
   CurveBox2:function (well_curve_config_template){
-    // function CurveBox2(well_curve_config_template){
+   
+      //// These parts of the function establish variables from the config JSON in shorter variable names
+      //// If there is a greater change that the template might not include them & they are necessary,
+      //// then a default or blank value is used
       well_curve_config_template = well_curve_config_template[0]
-      //let div_id = "well_holder"
-      if(well_curve_config_template["divID"]){div_id = well_curve_config_template["divID"]}
-      console.log("well_curve_config_template['divID']",well_curve_config_template["divID"])
+      
       let multipleLines = well_curve_config_template["multipleLines"]
+      
       let curveNames = well_curve_config_template["curveNames"]
       let curveColors = well_curve_config_template["curveColors"]
       let curveName = curveNames[0]
       let curveColor = curveColors[0]
-      ////
+      
       let curveUnits = "";
       if(well_curve_config_template["curveUnits"]){curveUnits = well_curve_config_template["curveUnits"]}
+      let div_id = "well_holder"
+      if(well_curve_config_template["divID"]){div_id = well_curve_config_template["divID"]}
+      ////
       let data = well_curve_config_template["data"]
       let width = well_curve_config_template["width"]
       let height = well_curve_config_template["height"]
@@ -214,6 +275,7 @@ takeInArraysAndGetObjectOfCurveDataForPlotting: function (arraysOfCurvesAndNames
       if(!depth_min){depth_min = d3.min(data, function(d) { return +d[depth_curve_name];});}
       let depth_max
       if(!depth_max){depth_max = d3.max(data, function(d) { return +d[depth_curve_name];});}
+      
       // Calculate x domain extent for one or more than one curve
       let mins = []
       let maxes = []
@@ -225,25 +287,27 @@ takeInArraysAndGetObjectOfCurveDataForPlotting: function (arraysOfCurvesAndNames
       }
       let min_all_curves = d3.min(mins)
       let max_all_curves = d3.max(maxes)
+    
       //// Calculate Axis & Scales
       let x = d3.scaleLinear().domain([min_all_curves,max_all_curves]).nice().range([margin.left, width - margin.right])
       let y = d3.scaleLinear().domain([depth_max, depth_min]).nice().range([height - margin.bottom, margin.top])
       let xAxis = g => g.attr("transform", `translate(0,${height - margin.bottom})`).call(d3.axisBottom(x).ticks(width / 80).tickSizeOuter(0))
       let yAxis = g => g.attr("transform", `translate(${margin.left},0)`).call(d3.axisLeft(y)).call(g => g.select(".domain").remove())
-      /////// All variables created above now they are applied below
-      /////// , some in for loops for multiple curves and areas
-     /////// attempt at function for color used in .....
+      
+      //// All variables created above now they are applied below
+      //// , some in for loops for multiple curves and areas
+      //// attempt at function for color used in .....
       function colors(data_array){
       let color_array = []
       for (let i =0;i<data_array.length;i++){
-         if (data[i][curveName] > 0){
-           color_array.push =("blue")
-         } 
-         else {
-           color_array.push("green")
-         }
-       }
-       return color_array
+        if (data[i][curveName] > 0){
+          color_array.push =("blue")
+        } 
+        else {
+          color_array.push("green")
+        }
+      }
+      return color_array
       }
       const svg = d3.select("#"+div_id).append("svg")
       svg.attr("class","first")
@@ -253,16 +317,16 @@ takeInArraysAndGetObjectOfCurveDataForPlotting: function (arraysOfCurvesAndNames
           .call(xAxis);
       svg.append("g")
           .call(yAxis);
-     /////// throw away code for single curve to plot that will be deleted soon  
-     ///// was here:
-    //// Code that assumes multiple curves are plotted in same curvebox   
-      if(multipleLines == "yes"){
-        for (let k = 0; k < curveNames.length; k++) {
-          ///// code that creates a line for each Curve in order provided and applies 
-          ///// the color in the color array in order provided
-          console.log("curveBox2 curveNames[k]",curveNames[k])
-          let another_line = d3.line().x(d => x(d[curveNames[k]])).y(d => y(d[depth_curve_name]));
-          svg.append("path")
+      //// throw away code for single curve to plot that will be deleted soon  
+      //// was here:
+      //// Code that assumes multiple curves are plotted in same curvebox   
+    for (let k = 0; k < curveNames.length; k++) {
+    //// code that creates a line for each Curve in order provided and applies 
+    //// the color in the color array in order provided
+      let another_line = d3.line().x(d => x(d[curveNames[k]])).y(d => y(d[depth_curve_name]));
+      let curveUnit = "";
+      if (curveUnits[k]){curveUnit = curveUnits[k]}   
+      svg.append("path")
           .datum(data)
           .attr("fill", "none")
           .attr("stroke", curveColors[k])
@@ -270,33 +334,44 @@ takeInArraysAndGetObjectOfCurveDataForPlotting: function (arraysOfCurvesAndNames
           .attr("stroke-linejoin", "round")
           .attr("stroke-linecap", "round")
           .attr("d", another_line);
-
-          //// This appends the units on top the curve box. Multiple units are possible. 
-          svg.append("text")
-            .attr("x", (width / 2))             
-            .attr("y", 0 + (margin.top -10 ))
-            .attr("text-anchor", "middle")  
-            .style("font-size", "16px") 
-            .style("text-decoration", "underline")  
-            .text(curveUnit);
-        } 
+          
+        let distanceFromTop = -10
+        if(k > 0){distanceFromTop = -30}
+        svg.append("text")
+          .attr("x", (width -40))             
+          .attr("y", 0 + (margin.top + distanceFromTop))
+          .attr("text-anchor", "middle")  
+          .style("font-size", "16px") 
+          .style("text-decoration", "underline")  
+          .text(curveUnit);
+      
+        
+        svg.append("text")
+          .attr("x", 40)             
+          .attr("y", 0 + (margin.top + distanceFromTop))
+          .attr("text-anchor", "middle")  
+          .style("font-size", "16px") 
+          .style("text-decoration", "underline")  
+          .text(curveNames[k]);
+        }
+        
       // define the area filled under the curve
-         let two_curve_fill_flag = "no"
-         for (let i = 0; i < well_curve_config_template["fill"].length; i++) {
-           ////
-           if (well_curve_config_template["fill"][i]["fill"] == "yes"){        
-             let number_colors = well_curve_config_template["fill"][i]["fillColors"].length
-             let curveName1 = well_curve_config_template["fill"][i]["curveName"]
-             let threshold = -99999999
-             let fillColor = "gray"
-             //////
-             for (let j = 0; j < number_colors; j++) {
-             console.log("got to start of J loop",j)
+        let two_curve_fill_flag = "no"
+        for (let i = 0; i < well_curve_config_template["fill"].length; i++) {
+          ////
+          if (well_curve_config_template["fill"][i]["fill"] == "yes"){        
+            let number_colors = well_curve_config_template["fill"][i]["fillColors"].length
+            let curveName1 = well_curve_config_template["fill"][i]["curveName"]
+            let threshold = -99999999
+            let fillColor = "gray"
+            //////
+            for (let j = 0; j < number_colors; j++) {
+            console.log("got to start of J loop",j)
                 let area1 = d3.area()
                 if (number_colors != 0){
-                   threshold = well_curve_config_template["fill"][i]["cutoffs"][j]
-                   fillColor = well_curve_config_template["fill"][i]["fillColors"][j]
-                   }
+                  threshold = well_curve_config_template["fill"][i]["cutoffs"][j]
+                  fillColor = well_curve_config_template["fill"][i]["fillColors"][j]
+                  }
                 if(well_curve_config_template["fill"][i]["fillDirection"] == "left"){
                     let startFromLeft = well_curve_config_template["margin"]["left"]
                     area1
@@ -304,55 +379,43 @@ takeInArraysAndGetObjectOfCurveDataForPlotting: function (arraysOfCurvesAndNames
                         .x0(d => startFromLeft)
                           .defined(d => ((d[curveName1])>threshold))
                         .y(d => y(d[depth_curve_name]));
-                 }
-                 if(well_curve_config_template["fill"][i]["fillDirection"] == "right"){
-                     let startFromRight = well_curve_config_template["margin"]["right"]
-                     area1
+                }
+                if(well_curve_config_template["fill"][i]["fillDirection"] == "right"){
+                    let startFromRight = well_curve_config_template["margin"]["right"]
+                    area1
                           .x0(d => x(d[curveName1]))
                           .x1(d => startFromRight)
                             .defined(d => ((d[curveName1])<threshold))
                           .y(d => y(d[depth_curve_name]));
-                 }
-                 if(well_curve_config_template["fill"][i]["fillDirection"] == "between"){
-                     let between2Curve = well_curve_config_template["fill"][i]["curve2"] 
-                     area1
+                }
+                if(well_curve_config_template["fill"][i]["fillDirection"] == "between"){
+                    let between2Curve = well_curve_config_template["fill"][i]["curve2"] 
+                    area1
                       .x1(d => x(d[curveName1]))
                       .x0(d => x(d[between2Curve]))
                         .defined(d => ((d[curveName1])>threshold))
                       .y(d => y(d[depth_curve_name]));
-                 }
-                 svg.append("path")      
-                       .datum(data)
-                       .attr("class", "area")
-                       .attr("d", area1)
-                       .attr("stroke", "none")
-                       .attr("fill",fillColor)
-                       .attr("fill-opacity",0.8);
-               console.log("got to end of J loop",j)
-             }
-           }
-         }
+                }
+                svg.append("path")      
+                      .datum(data)
+                      .attr("class", "area")
+                      .attr("d", area1)
+                      .attr("stroke", "none")
+                      .attr("fill",fillColor)
+                      .attr("fill-opacity",0.8);
+                
+                }
+          }
       }
-      // d3.select("#"+div_id).append(svg)
-      // d3.select("#"+div_id).append('div').html(svg)
-      console.log("got to bottom of curvebox2 function")
-      
       return svg.node();
     },
-    /////// this require wellio!
-    fileToJSON:function (afile){
-      return wellio.las2json(afile)
-    },
-    /////////
-    turnFilesIntoTextIntoWellioJSON:function (files_array){
-      //// For each well log file, turn into text, then convert text into wellio style JSON using wellio.js
-      let logs_in_json = []
-      for (let i = 0; i < files_array.length; i++) {
-        logs_in_json.push(fileToJSON(files_array[i]))
-      }
-      return logs_in_json
-    },
+    
     //////////
+    /**
+     * This function is used to plot multiple curveboxes in a row. AKA makes a cross-section. It calls curveBox multiple times.
+     * @param {string} divIDstring a string that represents the div ID that the multiple curveboxes will be appended to
+     * @param {Object} templates An array of CurveBox input templates
+     */
     multipleLogPlot:function(divIDstring,templates){
     // function multipleLogPlot(divIDstring,templates){
       //const noSVG = d3.select("#"+divIDstring).selectAll("svg").remove()
